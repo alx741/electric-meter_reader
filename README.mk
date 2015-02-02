1967e6909fcf4bbb4f4737370bc0b8a26d8fd3a2
Daniel Alejandro Campoverde Carrión [alx741] <alx741@riseup.net>
2015-02-01 21:24:03 -0500

    Hardware and Software sections added to README file.

--- README.mk
+++ README.mk
@@ -5,10 +5,35 @@ Electric Meter Reader
 What?
 -----
 This is a hardware and software hack for retrieving a home electric meter
-meassurement to the PC.
+measurements to the PC.
 
 The target meter has no identification, manufacturer name or model on it, so
-the only way the indentify it is by pictures.
+the only way the identify it is by pictures.
 
 
 TODO: upload some pics
+
+
+
+Hardware
+--------
+
+*Electric Meter*:
+	- PIC16F630 microcontroller
+	- HT1621B LCD controller
+	- ADE775 Electrical meassurements chip
+
+*Hardware Hack*:
+	- PIC16F628A microcontroller
+	- FTDI232 for usb/serial communication with the PC
+
+
+
+Software
+--------
+- The firmware software is written in C language for the SDCC compiler.
+
+- The host PC/Hardware hack communication software is written in C for *NIX OS
+  with Java wrappers.
+
+**Note that this project is *NIX OS only compatible**
