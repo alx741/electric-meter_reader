a9d46c92b8249e5524fc1f086c048168373df9d1
Daniel Alejandro Campoverde Carrión [alx741] <alx741@riseup.net>
2015-02-01 21:06:58 -0500

    First README file added.
    Describe what the project is.

---
+++ README.mk
@@ -0,0 +1,14 @@
+Electric Meter Reader
+=====================
+
+
+What?
+-----
+This is a hardware and software hack for retrieving a home electric meter
+meassurement to the PC.
+
+The target meter has no identification, manufacturer name or model on it, so
+the only way the indentify it is by pictures.
+
+
+TODO: upload some pics
